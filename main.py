65af1569e86f3a714b9762409148d2b8a913f291
Sviatoslav König <reveni324@gmail.com>
2025-02-06 08:36:10 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,9 @@
-# main.py
+import logging
 from gui import create_gui
 
-if __name__ == "__main__":
+def main():
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
     create_gui()
+
+if __name__ == "__main__":
+    main()
